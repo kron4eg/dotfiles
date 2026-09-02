abdfa6f77daa0fd15ce31aa246e268711e9342c8
Artiom Diomin <kron82@gmail.com>
2026-09-02 15:16:20 +0300

    always show hidden and ignored files

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
+    },
+  },
+}
